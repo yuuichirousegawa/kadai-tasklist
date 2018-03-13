40b026c44ebad03430befffa2e12e2633c8b213a
yuichiro.segawa <segawayuichiro@gmail.com>
2018-03-13 12:40:20 +0000

    login update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    email = params[:session][:email].downcase
-    password = params[:session][:password]
-    if login(email, password)
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
       flash[:success] = 'ログインしました'
-      redirect_to @user
+      redirect_to user
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render 'new'
@@ -18,19 +18,5 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = 'ログアウトしました'
     redirect_to root_path
-  end
-  
-  private
-  
-  def login(email, password)
-    @user = User.find_by(email: email)
-    if @user && @user.authenticate(password)
-      # ログイン成功
-      session[:user_id] = @user.id
-      return true
-    else
-      # ログイン失敗
-      return false
-    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
   
     if @user.save
+      log_in @user
     flash[:success] = 'ユーザー登録に成功しました'
     redirect_to @user
     else
